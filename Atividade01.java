8045c26a0e4896dfd75a5e0169d09b0b491a345c
DudaSantoni <159277791+DudaSantoni@users.noreply.github.com>
2024-03-13 12:16:48 -0300

    atividade1303

---
+++ Atividade01.java
@@ -0,0 +1,14 @@
+package atividade1303;
+
+public class Atividade01 {
+
+	public static void main(String[] args) {
+		String frase = "Tedro Tescava Teixes no Tíer";
+		String corrigir = frase.replaceAll("T", "P");
+		
+		System.out.println("Antes: "+frase +" e depois: " + corrigir);
+		
+
+	}
+
+}
---
+++ Atividade02.java
@@ -0,0 +1,11 @@
+package atividade1303;
+
+public class Atividade02 {
+
+	public static void main(String[] args) {
+		String frase = "Pedro Pescava Peixes no Píer";
+		String palavra = frase.substring(14, 21);
+		System.out.println("Palavra extraída: " + palavra);
+	}
+
+}
